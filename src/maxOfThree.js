0f4eea843ee02b7a3aae3f429a6b42d727a96629
Simon Haines <nimon@hotmail.co.uk>
2024-06-02 23:17:35 +0100

    solves rest of problems

--- src/maxOfThree.js
+++ src/maxOfThree.js
@@ -1,13 +1,13 @@
 function maxOfThree(x, y, z) {
-  if (x > y && x > z) {
+  if (x >= y && x >= z) {
     return x
   }
 
-  if (y > z && y > x) {
+  if (y >= z && y >= x) {
     return y
   }
 
-  if (z > x && z > y) {
+  if (z >= x && z >= y) {
     return z
   }
 }
